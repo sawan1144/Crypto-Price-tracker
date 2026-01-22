0db5d64e2cd071d72c56ddbe0eebbcd2c963e73b
sawan44 <sawanpanwarx@gmail.com>
2026-01-22 23:48:25 +0530

    searching and routing added

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,14 +1,40 @@
+import { useContext, useEffect, useState } from "react"
+import { CoinContext } from "../context/CoinContext"
+import {Link} from "react-router-dom"
 
 const Table = () => {
+  const {allcoin, currency, searchVal} = useContext(CoinContext);
+  const [displayCoin, setdisplayCoin] = useState([])
+
+  useEffect(()=>{
+    setdisplayCoin(allcoin);
+  },[allcoin])
+
+  const searchedCoin = displayCoin.filter((item)=>item.name.toLowerCase().includes(searchVal.toLowerCase()));
+
   return (
-    <div className='px-60 py-10'>
-        <div className='grid grid-cols-[0.5fr_2fr_1fr_1fr_1.5fr] bg-[#121212] p-5 rounded-xl border border-gray-600'>
+    <div className='px-60 pt-10 pb-15'>
+      <div className=" rounded-xl border border-gray-600 bg-[#101010]">
+        <div className='grid grid-cols-[0.5fr_2fr_1fr_1fr_1.5fr] p-5 pb-8 rounded-xl text-sm text-gray-400'>
             <div>#</div>
             <div>Coin</div>
             <div>Price</div>
-            <div>24h Change</div>
+            <div className="text-center">24h Change</div>
             <div className='text-right'>Market Cap</div>
         </div>
+        {(searchVal ? searchedCoin : displayCoin.slice(0, 10)).map((data, index)=>(
+          <Link to={`/coin/${data.id}`} key={index} className='grid grid-cols-[0.5fr_2fr_1fr_1fr_1.5fr] p-5 rounded-xl items-center'>
+            <div>{data.market_cap_rank}</div>
+            <div className="flex items-center gap-2"><img src={data.image} className="h-8"/><p>{data.name + " - " + data.symbol}</p></div>
+            <div className="font-semibold">{currency.symbol + " " + data.current_price.toLocaleString()}</div>
+
+            <div className={`text-center ${data.price_change_percentage_24h > 0 ? "text-green-500" : "text-red-500"}`}>
+              {data.price_change_percentage_24h.toFixed(2)}% </div>
+
+            <div className='text-right'>{currency.symbol + " " + data.market_cap.toLocaleString()}</div>
+        </Link>
+        ))}
+      </div>  
     </div>
   )
 }
