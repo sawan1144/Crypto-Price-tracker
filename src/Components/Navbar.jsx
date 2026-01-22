de35cf4a76cf9b013576ef75c0cad0eea099c91b
sawan44 <sawanpanwarx@gmail.com>
2026-01-22 23:45:14 +0530

    routes-added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { HiArrowUpRight, HiChevronDown } from 'react-icons/hi2'
 import { CoinContext } from '../context/CoinContext'
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     const [show, setShow] = useState(false)
@@ -14,14 +15,15 @@ const Navbar = () => {
     function currencyHandler(c, s){
         setCurrency({name: c, symbol: s})
         setcurr(c.toUpperCase())
+        setShow((p) => !p)
     }
 
     return (
         <div className='sticky top-0 z-50 backdrop-blur-xl bg-gray-500/25'>
             <div className='flex justify-between items-center py-4 px-25'>
-                <img src="/imgs/logo.png" alt="Logo" className='h-8' />
+                <a onClick={()=>window.location.href = "/"}><img src="/imgs/logo.png" alt="Logo" className='h-8'/></a> 
                 <div className='flex gap-10 text-sm font-semibold'>
-                    <button className='cursor-pointer'>Home</button>
+                    <Link to="/" className='cursor-pointer'>Home</Link>
                     <button className='cursor-pointer'>Features</button>
                     <button className='cursor-pointer'>Pricing</button>
                     <button className='cursor-pointer'>Blog</button>
