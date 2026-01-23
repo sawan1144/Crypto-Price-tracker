1dd13ea49a38fe4e2b6f7e16956dcc79a00ca595
sawan44 <sawanpanwarx@gmail.com>
2026-01-23 17:35:38 +0530

    loader done

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,11 @@
+
+function Loader() {
+
+  return (
+    <div className="flex items-center justify-center h-64">
+      <div className="h-8 w-8 animate-spin rounded-full border-2 border-gray-300 border-t-blue-500" />
+    </div>
+  );
+}
+
+export default Loader;
